1b09545bbb6b296960c6be738f7aead42826bbc2
zhhyang0404@163.com <jjyy0404>
2016-03-30 18:52:50 +0800

    add comments

--- routes/index.js
+++ routes/index.js
@@ -203,6 +203,29 @@ module.exports = function (app) {
             })
         })
     });
+
+    app.post('/u/:name/:day/:title',function (req,res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+                date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+        newComment.save(function (err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
+        })
+    });
+
     /**
      * 跳转到修改页面
      * */
